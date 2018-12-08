fddfb989d84c5eb63715dcc57ef9cd51fec9f19d
Josep Dols <jodoldar@users.noreply.github.com>
2018-12-08 16:30:53 +0100

    5.9 Created index, show, and db

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,6 +3,22 @@ class PlacesController < ApplicationController
     end
 
     def create
-        render plain: params[:place].inspect
+        @place = Place.new(place_params)
+
+        @place.save
+        redirect_to @place
     end
+
+    def show
+        @place = Place.find(params[:id])
+    end
+
+    def index
+        @places = Place.all
+    end
+
+    private
+        def place_params
+            params.require(:place).permit(:title, :text)
+        end
 end
