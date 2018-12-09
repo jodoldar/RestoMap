b3a4939e083b197dfeb4d3e86fbb7e8de406eea6
Josep Dols <jodoldar@users.noreply.github.com>
2018-12-09 23:12:17 +0100

    Points 9 & 10. Added some logs

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,16 @@
 class PlacesController < ApplicationController
     def new
+        @place = Place.new
     end
 
     def create
         @place = Place.new(place_params)
 
-        @place.save
-        redirect_to @place
+        if @place.save
+            redirect_to @place
+        else
+            render 'new'
+        end
     end
 
     def show
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,4 @@
 class Place < ApplicationRecord
+    validates :title, presence: true,
+                      length: { minimum: 2 }
 end
