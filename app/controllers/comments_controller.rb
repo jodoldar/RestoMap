7ed899f2111fbbbaa23a7d2812cc60cb3334501b
Josep <jodoldar@gmail.com>
2018-12-11 21:00:39 +0100

    Finished Comments basic functions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+    def create
+        @place = Place.find(params[:place_id])
+        @comment = @place.comments.create(comment_params)
+        redirect_to place_path(@place)
+    end
+
+    def destroy
+        @place = Place.find(params[:place_id])
+        @comment = @place.comments.find(params[:id])
+        @comment.destroy
+        redirect_to place_path(@place)
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:user, :comment)
+    end
 end
