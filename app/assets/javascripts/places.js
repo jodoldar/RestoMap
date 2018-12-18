10e184d66ef8b5fa9e48db45ae1bd18be69f3761
Josep <jodoldar@gmail.com>
2018-12-18 22:58:18 +0100

    Prueba ES6 9

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -25,6 +25,8 @@ function indexMap(lat, lng, places) {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    console.log(places);
 }
 
 var createMarker;
