99a25a2cfa96dc6ee6cab18ca1f58c4d4f97f2b2
Josep <jodoldar@gmail.com>
2019-01-02 00:38:39 +0100

    Fixes to codeClimate3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit( :name, :email, :password,
-                                  :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
