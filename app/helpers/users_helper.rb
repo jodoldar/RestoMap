ebeb1cccf5e71fd5b263ab750142e903b2fea22a
Josep <jodoldar@gmail.com>
2019-01-02 00:19:42 +0100

    Added first steps in User Management

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_uri = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=identicon"
+    image_tag(gravatar_uri, alt: user.name, class: "gravatar")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :places do
     resources :comments
   end
+  resources :users
 
   root 'welcome#index'
 end
