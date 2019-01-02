4ecb0788c07a9a4c0fb2a7a41304f8a8ccb39f65
Josep <jodoldar@gmail.com>
2019-01-02 23:11:40 +0100

    Better CI 3brd check

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,12 +7,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-class ActiveSupport
-  class TestCase
-    # Setup all fixtures in test/fixtures/*.yml for all tests in
-    # alphabetical order.
-    fixtures :all
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in
+  # alphabetical order.
+  fixtures :all
 
-    # Add more helper methods to be used by all tests here...
-  end
+  # Add more helper methods to be used by all tests here...
 end
